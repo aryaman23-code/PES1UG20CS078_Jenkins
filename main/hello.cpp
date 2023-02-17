0206dabeec033fab04c4615e89c15ea3e4fb9e86
Aryaman <79296574+aryaman23-code@users.noreply.github.com>
2023-02-17 10:24:54 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World I introduce you to PES1UG20CS078 \n";
+  cout << "PES1UG20CS078 \n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
